f2d86ba78541ca34f7a891d13810cebbe71ec58c
mikechavo <mchavez76@yahoo.com>
2023-09-11 16:37:30 -0700

    fetching started

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react" 
+import React, { useEffect, useState } from "react" 
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
@@ -9,13 +9,36 @@ import CharShow from "./pages/CharShow"
 import NotFound from "./pages/NotFound"
 import { Routes, Route } from "react-router-dom"
 
-import mockChars from "./mockChars"
+
 
 const App = () => {
 
-  const [chars, setChars] = useState(mockChars)
+  const [chars, setChars] = useState([])
+
+  useEffect(() => {
+    readChar()
+  }, [])
+
+  const readChar = () => {
+    fetch("http://localhost:3000/fictional_chars")
+    .then((response) => response.json())
+    .then((payload) => {
+      setChars(payload)
+    })
+    .catch((errors) => console.log(errors))
+  }
 
   const createChar = (char) => {
+    fetch("http://localhost:3000/fictional_chars", {
+      body: JSON.stringify(char),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    })
+      .then((response) => response.json())
+      .then((payload) => readChar())
+      .catch((errors) => console.log("lets see sum:", errors))
   }
 
   const updateChar = (char, id) => {
